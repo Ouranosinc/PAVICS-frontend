4b7c12d3d6dec16344ff29ed161b39b581da3bb2
beaulima <mario.beaulieu@crim.ca>
2016-07-05 17:21:43 -0400

    Version revisée (v1); Code refactoring;  Passage OL3.17.1

--- src/components/OLComponent/OLComponent.js
+++ src/components/OLComponent/OLComponent.js
@@ -1,13 +1,17 @@
 import React from 'react'
 //import classes from './CesiumComponent.scss'
 import ol from 'openlayers';
+require('./ol3-layerswitcher.js')
 import WMS from './'
 import $ from 'jquery';
 
 require("openlayers/css/ol.css");
 require("./OLComponent.css");
+import classes from './ol3-layerswitcher.scss'
 
-var me; //Do never trust this in javascript.
+
+var g_BING_API_KEY = 'AtXX65CBBfZXBxm6oMyf_5idMAMI7W6a5GuZ5acVcrYi6lCQayiiBz7_aMHB7JR7'
+
 
 class OLComponent extends React.Component {
   static propTypes = {
@@ -18,215 +22,201 @@ class OLComponent extends React.Component {
   constructor(props) {
     super(props);
     this.map = null;
-    this.layers = [];
-    this.BackgroundLayer = null;
-    this.WMSLayer = null;
-    this.WCSLayer = null;
-    this.WFSLayer = null;
-    this.vectorSource = null;
+    this.baseLayers = new ol.layer.Group({'title': 'Base maps', 'opacity':1.0, 'visible':true,'zIndex':0})
+    this.overlayLayers = new ol.layer.Group({'title': 'Overlays','opacity':1.0, 'visible':true,'zIndex':1})
+    this.view = null
+    this.tmpLayer=null
+
     this.popup = null;
 
-    me = this;
   }
 
-  setBackgroundLayer(){
-    this.BackgroundLayer = new ol.layer.Tile({
-      //source: new ol.source.OSM()
-      source: new ol.source.MapQuest({layer: 'sat'})
-    });
+  // Returns base layers list
+  getMapBaseLayersList() {
+    if (this.baseLayers != null) {
+      return this.baseLayers.getLayers()
+    }
+    return []
   }
 
-  setWMSLayer(){
-    //http://132.217.140.31:8080/ncWMS2/wms?
-    // FORMAT=image%2Fpng&
-    // TRANSPARENT=TRUE&
-    // STYLES=default-scalar%2Fdefault&
-    // LAYERS=pavics_dummy_file_simplest%2Fpr&
-    // TIME=1961-12-06T00%253A00%253A00.000Z&
-    // COLORSCALERANGE=0%2C1&
-    // NUMCOLORBANDS=250&
-    // ABOVEMAXCOLOR=0x000000&
-    // BELOWMINCOLOR=0x000000&
-    // BGCOLOR=transparent&
-    // LOGSCALE=false&
-    // SERVICE=WMS&
-    // VERSION=1.1.1&
-    // REQUEST=GetMap&
-    // SRS=EPSG%3A4326&
-    // BBOX=-78.88,45.68,-77.6,46.96&
-    // WIDTH=256&
-    // HEIGHT=256
-
-    /*this.WMSLayer = new ol.layer.Tile({
-      extent: [-13884991, 2870341, -7455066, 6338219],
-      source: new ol.source.TileWMS({
-        url: 'http://132.217.140.31:8080/ncWMS2/wms',
-        params: {
-          //'FORMAT': 'image/png',
-          'TRANSPARENT': 'TRUE',
-          //'STYLES': 'default-scalar/default',
-          'LAYERS' : 'pavics_dummy_file_simplest/pr',
-          //'COLORSCALERANGE' : '0,1',
-          //'NUMCOLORBANDS' : '250',
-          //'ABOVEMAXCOLOR' : '0x000000',
-          //'BELOWMINCOLOR' : '0x000000',
-          'BGCOLOR' : 'transparent',
-          //'LOGSCALE' : 'false',
-          //'SERVICE' : 'WMS',
-          //'VERSION' : '1.1.1',
-          //'REQUEST' : 'GetMap',
-          'SRS' : 'PSG:4326'//,
-          //'BBOX' : '-78.88,45.68,-77.6,46.96',
-          //'WIDTH': '256',
-          //'HEIGHT': '256'
-        }
-      })
-    });*/
-
-    /*if(this.props.dataset){
-      this.WMSLayer = new ol.layer.Tile({
-        extent: [-13884991, 2870341, -7455066, 6338219],
-        visible: true,
-        source: new ol.source.TileWMS({
-          url: 'http://132.217.140.31:8080/ncWMS2/wms',
-          params: {
-            'TRANSPARENT': 'TRUE',
-            'LAYERS' : 'pavics_dummy_file_simplest/pr',
-            'BGCOLOR' : 'transparent',
-            'SRS' : 'PSG:4326'
-
-          }
-        })
-      });
-    }else{*/
-      //Default
-      this.WMSLayer = new ol.layer.Tile({
-        //extent: [-91.00001018206,87.99997963588,-46.50001527309,132.49997454485],
-        //extent: [-13884991, 2870341, -7455066, 6338219],
-        visible: false,
-        source: new ol.source.TileWMS({
-          url: 'http://demo.boundlessgeo.com/geoserver/wms',
-          params: {'LAYERS': 'topp:states', 'TILED': true},
-          serverType: 'geoserver'
-        })
-      });
-    /*}*/
+  // Returns overlay layers list
+  getMapOverlayList() {
+    if (this.overlayLayers != null) {
+      return this.overlayLayers.getLayers()
+    }
+    return []
   }
 
-  setWCSLayer(){
-    //http://gis.stackexchange.com/questions/166868/does-openlayers-3-support-wcs
-    this.WCSLayer = new ol.layer.Image({
-      //extent: [-13884991, 2870341, -7455066, 6338219],
-      source: new ol.source.ImageWMS({
-        url: 'http://demo.boundlessgeo.com/geoserver/wms',
-        params: {'LAYERS': 'topp:states'},
-        serverType: 'geoserver'
-      })
-    });
+  // Add backgrounnd layer (use once)
+  initBackgroundLayer() {
+    this.addBingLayer('Aerial', this.getMapBaseLayersList(),'Aerial')
+    var wmsUrl = "http://demo.boundlessgeo.com/geoserver/wms";
+    var wmsParams = {'LAYERS': 'topp:states', 'TILED': true};
+    this.addTileWMSLayer('topp:states', this.getMapOverlayList(), wmsUrl, wmsParams);
   }
 
-  setWFSVector(){
-    /*this.vectorSource = new ol.source.Vector({
-      loader: function(extent, resolution, projection) {
-        var url = 'http://demo.boundlessgeo.com/geoserver/wfs?service=WFS&' +
-          'version=1.1.0&request=GetFeature&typename=osm:water_areas&' +
-          'outputFormat=text/javascript&format_options=callback:loadFeatures' +
-          '&srsname=EPSG:3857&bbox=' + extent.join(',') + ',EPSG:3857';
-        $.ajax({
-          url: url,
-          dataType: 'jsonp',
-          jsonp: false});
-      },
-      strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
-        maxZoom: 19
-      }))
-    });
-    this.WFSLayer = new ol.layer.Vector({
-      source: this.vectorSource,
-      style: new ol.style.Style({
-        stroke: new ol.style.Stroke({
-          color: 'rgba(0, 0, 255, 1.0)',
-          width: 2
-        })
-      })
-    });*/
-  }
+  removeLayer(layers, title){
+    var layer;
 
-  initMap(){
-    /**
-     * JSONP WFS callback function.
-     * @param {Object} response The response object.
-     */
-    window.loadFeatures = function(response) {
-      me.vectorSource.addFeatures(new ol.format.GeoJSON().readFeatures(response));
-    };
-
-    this.setBackgroundLayer();
-    this.setWMSLayer();
-    this.setWCSLayer();
-    this.setWFSVector();
-
-    this.layers = [
-      this.BackgroundLayer,
-      this.WMSLayer
-    ];
-    this.map = new ol.Map({
-      layers: this.layers,
-      target: 'map',
-      view: new ol.View({
-        center: [-10997148, 9569099],
-        zoom: 4
-        /*center: [-10997148, 4569099],
-        zoom: 4*/
-      }),
-      controls: ol.control.defaults().extend([
-        /*new ol.control.ZoomSlider(),*/
-        new ol.control.Rotate(),
-        new ol.control.OverviewMap(),
-        new ol.control.ScaleLine(),
-        new ol.control.FullScreen(),
-        new ol.control.MousePosition({
-          coordinateFormat: ol.coordinate.createStringXY(4),
-          projection: 'EPSG:4326'
-        })
-      ]),
-      interactions: ol.interaction.defaults().extend([
-        new ol.interaction.DragRotateAndZoom(),
-        new ol.interaction.Select({
-          condition: ol.events.condition.mouseMove
-        })
-      ])
-    });
-
-    this.map.on('pointermove', function (e) {
-      var data = {
-        pixel: me.map.getEventPixel(e.originalEvent),
-        last: e.map.last_tile_overlay
-      };
-      console.log('pointermove event');
-    });
-
-    this.map.on('moveend', function (e) {
-      var data = {
-        center: me.map.getView().getCenter(),
-        resolution: me.map.getView().getResolution()
-      };
-      console.log('moveend event');
-      //me.initMap();
-    });
-
-    var popupElement = document.getElementById('popup');
-    this.popup = new ol.Overlay({
-      element: popupElement,
-      autoPan: true,
-      autoPanAnimation: {
-        duration: 250
+    for(layer in layers)
+      if(title===layer.get('title')) {
+        console.log('addTileWMSLayer: First Remove layer ' + layer.get('title'));
+        this.map.removeLayer(layer)
       }
+  }
+
+  /*! \brief Adds a layer to a layers list
+   @param layers layers input list
+   @param wmsUrl wms url
+   @param wmsParams parameters associated to the wms
+   @param extent region extent to load
+   @param serverType Server's type
+   */
+  addTileWMSLayer(title,
+                  layers,
+                  wmsUrl,
+                  wmsParams,
+                  extent,
+                  serverType,
+                  visible=true) {
+
+    var layer = this.getTileWMSLayer(title,
+      wmsUrl,
+      wmsParams,
+      extent,
+      serverType,
+      visible)
+    layers.push(layer)
+    console.log('addTileWMSLayer: Add layer ' + layer.get('title'));
+    return layer;
+  }
+
+  /*! \brief Returns a ol3 layer to a layers list
+   @param layers layers input list
+   @param wmsUrl wms url
+   @param wmsParams parameters associated to the wms
+   @param extent region extent to load
+   @param serverType Server's type
+   */
+  getTileWMSLayer(title,
+                  wmsUrl,
+                  wmsParams,
+                  extent,
+                  serverType="",
+                  visible=true){
+
+    if (extent == undefined) {
+        return new ol.layer.Tile(
+          {
+            visible:visible,
+            title: title,
+            source: new ol.source.TileWMS(
+              {
+                url: wmsUrl,
+                params: wmsParams,
+                serverType: serverType
+              })
+          });
+    }
+    else {
+        return new ol.layer.Tile(
+          {
+            title: title,
+            extent: extent,
+            source: new ol.source.TileWMS(
+              {
+                url: wmsUrl,
+                params: wmsParams,
+                serverType: serverType
+              })
+          });
+    }
+  }
+
+  addBingLayer(title, layers, bingStyle){
+    layers.push(new ol.layer.Tile({
+      visible: true,
+      preload: Infinity,
+      source: new ol.source.BingMaps({
+        key: g_BING_API_KEY,
+        imagerySet: bingStyle
+        // use maxZoom 19 to see stretched tiles instead of the BingMaps
+        // "no photos at this zoom level" tiles
+        // maxZoom: 19
+      })
+    }));
+  }
+
+  initMap() {
+
+    this.view = new ol.View({
+      center: [-10997148, 4569099],
+      zoom: 4
+    })
+
+    var map = new ol.Map({
+      layers: [this.baseLayers, this.overlayLayers],
+      target: 'map',
+      renderer: 'canvas',
+      view: this.view
     });
-    this.map.addOverlay(this.popup);
+
+    var layerSwitcher = new ol.control.LayerSwitcher({
+      tipLabel: 'Legend' // Optional label for button
+    });
+    map.addControl(layerSwitcher);
+
+    this.map = map;
+  }
+
+  /** Returns view resolution */
+  getCurrentResolution() {
+    if (this.view != null) {
+      return this.view.getResolution()
+    }
+    return -1;
+  }
+
+  /** Sets view resolution */
+  setCurrentResolution(resolution) {
+    if (this.view != null) {
+      this.view.setResolution(resolution)
+    }
+  }
+
+  /** Returns current view center */
+  getCurrentCenter() {
+    if (this.view != null) {
+      return this.view.getCenter()
+    }
+    return [];
+  }
+
+  /** Sets current view center */
+  setCurrentCenter(center) {
+    if (this.view != null) {
+      this.view.setCenter(center)
+    }
+    return [];
+  }
+
+  /** Returns current projection */
+  getCurrentProjection() {
+    if (this.view != null) {
+      return this.view.getProjection()
+    }
+    return "";
+  }
+
+  /** Sets current projection */
+  setCurrentProjection(epsg_string) {
+    if (this.view != null) {
+      this.view.setProjection(epsg_string)
+    }
   }
 
   componentDidMount(){
+    this.initBackgroundLayer()
     this.initMap();
   }
 
@@ -239,18 +229,21 @@ class OLComponent extends React.Component {
   componentDidUpdate(prevProps, prevState){
     if(this.props.dataset && this.props.capabilities){
       var wmsUrl = this.props.capabilities.value["WMS_Capabilities"]["Service"][0]["OnlineResource"][0]["$"]["xlink:href"];
-      this.WMSLayer.setSource(new ol.source.TileWMS({
-        url: wmsUrl,
-        params: {
-          'TRANSPARENT': 'TRUE',
-          'LAYERS' : this.props.dataset["Name"][0],
-          'BGCOLOR' : 'transparent',
-          'SRS' : 'PSG:4326'
-        }
-      }));
-      this.WMSLayer.set("visible", true);
+      var wmsParams = {
+        'TRANSPARENT': 'TRUE',
+        'LAYERS' : this.props.dataset["Name"][0],
+        'BGCOLOR' : 'transparent',
+        'SRS' : 'PSG:4326'
+      }
+      //this.addTileWMSLayer(this.props.dataset["Name"][0],this.getMapOverlayList(), wmsUrl, wmsParams);
+      if(this.tmpLayer)
+      {
+        this.tmpLayer.setVisible(false);
+        this.map.removeLayer(this.tmpLayer);
+      }
+
+      this.tmpLayer = this.addTileWMSLayer("ncWMS2 test",this.getMapOverlayList(), wmsUrl, wmsParams);
     }else{
-      this.WMSLayer.set("visible", false);
     }
   }
 
