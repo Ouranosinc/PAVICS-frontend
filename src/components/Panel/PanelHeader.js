80b70a6f83487b6d13768a41c2b4ee504f7346e8
Félix Gagnon-Grenier <fractalesque@gmail.com>
2016-09-20 15:50:22 -0400

    PAV-157 - fix - toggle icon size

--- src/components/Panel/PanelHeader.js
+++ src/components/Panel/PanelHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {ToggleButton} from '../Panel'
+import style from './Panel.scss'
 class PanelHeader extends React.Component {
   static propTypes = {
     onClick: React.PropTypes.func.isRequired,
@@ -9,7 +10,8 @@ class PanelHeader extends React.Component {
   {
     return (
       <div>
-        <h3><ToggleButton onClick={this.props.onClick} icon={this.props.icon}/>{this.props.children}</h3>
+        <ToggleButton onClick={this.props.onClick} icon={this.props.icon}/>
+        <span className={style['header']}>{this.props.children}</span>
       </div>
     );
   }
