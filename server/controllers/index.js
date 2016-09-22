5bfd183bdea1e94bdcd28fdcef564003294c58a5
Félix Gagnon-Grenier <fractalesque@gmail.com>
2016-09-22 11:40:02 -0400

    style - ending lines and beautiful import

---
+++ server/controllers/index.js
@@ -0,0 +1,10 @@
+import birdhouse from './birdhouse'
+import datasets from './datasets'
+import facets from './facets'
+import wms from './wms'
+export {
+  birdhouse,
+  datasets,
+  facets,
+  wms,
+}
--- server/main.js
+++ server/main.js
@@ -10,48 +10,37 @@ import _debug from 'debug'
 import config from '../config'
 import webpackDevMiddleware from './middleware/webpack-dev'
 import webpackHMRMiddleware from './middleware/webpack-hmr'
-
-const debug = _debug('app:server')
+const debug = _debug('app:server');
 const paths = config.utils_paths;
 const app = new Koa();
-
 //Controllers
-var birdhouse = require('./controllers/birdhouse');
-var facets = require('./controllers/facets');
-var datasets = require('./controllers/datasets');
-var wms = require('./controllers/wms');
-
+import {birdhouse, datasets, facets, wms} from './controllers'
 //Routes
 app.use(route.get('/api/wms/capabilities', birdhouse.getCapabilities));
 app.use(route.get('/api/wms/dataset/layers', wms.getLayers));
 app.use(route.get('/api/facets', facets.getFacets));
 app.use(route.get('/api/datasets', datasets.getDatasets));
 app.use(route.get('/api/dataset', datasets.getDataset));
-
 // Enable koa-proxy if it has been enabled in the config.
+// Because it's been enabled, so I guess we should enable it
 if (config.proxy && config.proxy.enabled) {
   app.use(convert(proxy(config.proxy.options)))
 }
-
 // This rewrites all routes requests to the root /index.html file
 // (ignoring file requests). If you want to implement isomorphic
 // rendering, you'll want to remove this middleware.
 app.use(convert(historyApiFallback({
   verbose: false
-})))
-
+})));
 // ------------------------------------
 // Apply Webpack HMR Middleware
 // ------------------------------------
 if (config.env === 'development') {
-  const compiler = webpack(webpackConfig)
-
+  const compiler = webpack(webpackConfig);
   // Enable webpack-dev and webpack-hot middleware
-  const { publicPath } = webpackConfig.output
-
-  app.use(webpackDevMiddleware(compiler, publicPath))
-  app.use(webpackHMRMiddleware(compiler))
-
+  const {publicPath} = webpackConfig.output;
+  app.use(webpackDevMiddleware(compiler, publicPath));
+  app.use(webpackHMRMiddleware(compiler));
   // Serve static assets from ~/src/static since Webpack is unaware of
   // these files. This middleware doesn't need to be enabled outside
   // of development since this directory will be copied into ~/dist
@@ -64,12 +53,10 @@ if (config.env === 'development') {
     'do not need an application server for this and can instead use a web ' +
     'server such as nginx to serve your static files. See the "deployment" ' +
     'section in the README for more information on deployment strategies.'
-  )
-
+  );
   // Serving ~/dist by default. Ideally these files should be served by
   // the web server and not the app server, but this helps to demo the
   // server in production.
   app.use(convert(serve(paths.dist())))
 }
-
 export default app
