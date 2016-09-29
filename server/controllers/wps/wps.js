819aaa880c14718e3bb8e1c450b70d9a118e79be
Félix Gagnon-Grenier <fractalesque@gmail.com>
2016-09-29 16:45:28 -0400

    PAV-166 - feature - data from wps getCapabilities

--- server/controllers/wps/wps.js
+++ server/controllers/wps/wps.js
@@ -1,21 +1,16 @@
-var wps = (function() {
-  console.log("this is happening");
+import request from 'koa-request'
+import Utils from './../../Utils'
+var wps = (function () {
   return {
     getClimateIndicators: function *() {
-      this.body = [
-        {
-          key: "key1",
-          value: "value1"
-        },
-        {
-          key: "key2",
-          value: "value2"
-        },
-        {
-          key: "key3",
-          value: "value3"
-        },
-      ];
+      // hardcoded for now until we know for sure where these capabilities will be coming from
+      let response = yield request("http://132.217.140.31:8092/wps?service=WPS&version=1.0.0&request=DescribeProcess&identifier=cdo_operation");
+      let json = yield Utils.parseXMLThunk(response.body);
+      let inputs = json["wps:ProcessDescriptions"]["ProcessDescription"][0]["DataInputs"][0]["Input"];
+      let allowedValues = inputs[1]["LiteralData"][0]["ows:AllowedValues"][0]["ows:Value"];
+      this.body = allowedValues.map((x, i) => {
+        return {key: i, value: x};
+      });
     }
   };
 })();
