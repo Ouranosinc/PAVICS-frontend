5881a02e62fb13f49cbb5daa717f8e995d8647f0
osboxes.org <osboxes@osboxes.corpo.crim.ca>
2016-11-08 20:55:41 +0000

    Added days to month slider

--- src/containers/TimeSlider/TimeSlider.js
+++ src/containers/TimeSlider/TimeSlider.js
@@ -4,7 +4,9 @@ require('rc-slider/assets/index.css');
 import classes from './TimeSlider.scss'
 import Slider  from 'rc-slider';
 import { Button, Col, ControlLabel, Form, FormControl, FormGroup, Glyphicon } from 'react-bootstrap'
-import Loader from '../../components/Loader'
+import Loader from '../../components/Loader';
+
+const DIVIDER = 1000;
 
 export class TimeSlider extends React.Component {
   static propTypes = {
@@ -17,11 +19,13 @@ export class TimeSlider extends React.Component {
     this._onChangedStepGranularity = this._onChangedStepGranularity.bind(this);
     this._onChangedStepLength = this._onChangedStepLength.bind(this);
     this._onChangedStepSpeed = this._onChangedStepSpeed.bind(this);
+    this._onChangedMonthSlider = this._onChangedMonthSlider.bind(this);
+    this._onChangedMonthSlider = this._onChangedMonthSlider.bind(this);
     this._onSelectedDay = this._onSelectedDay.bind(this);
     this._onSelectedHour = this._onSelectedHour.bind(this);
     this._onSelectedMinute = this._onSelectedMinute.bind(this);
     this.state = {
-      current: '1960-05-01 00:00',
+      current: '1960-05-01',
       stepLength: 10,
       stepGranularity: 'minute',
       stepSpeed: 1
@@ -30,20 +34,19 @@ export class TimeSlider extends React.Component {
 
 
   render() {
-    let marksMonths = {
-      1: 'Jan',
-      2: 'Feb',
-      3: 'Mar',
-      4: 'Apr',
-      5: 'May',
-      6: 'Jun',
-      7: 'Jul',
-      8: 'Aug',
-      9: 'Sept',
-      10: 'Oct',
-      11: 'Nov',
-      12: 'Dec'
-    };
+    let marksMonths = {};
+    marksMonths[new Date(2016, 1, 1).valueOf() / DIVIDER] = 'Jan';
+    marksMonths[new Date(2016, 2, 1).valueOf() / DIVIDER] = 'Feb';
+    marksMonths[new Date(2016, 3, 1).valueOf() / DIVIDER] = 'Mar';
+    marksMonths[new Date(2016, 4, 1).valueOf() / DIVIDER] = 'Apr';
+    marksMonths[new Date(2016, 5, 1).valueOf() / DIVIDER] = 'May';
+    marksMonths[new Date(2016, 6, 1).valueOf() / DIVIDER] = 'Jun';
+    marksMonths[new Date(2016, 7, 1).valueOf() / DIVIDER] = 'Jul';
+    marksMonths[new Date(2016, 8, 1).valueOf() / DIVIDER] = 'Aug';
+    marksMonths[new Date(2016, 9, 1).valueOf() / DIVIDER] = 'Sept';
+    marksMonths[new Date(2016, 10, 1).valueOf() / DIVIDER] = 'Oct';
+    marksMonths[new Date(2016, 11, 1).valueOf() / DIVIDER] = 'Nov';
+    marksMonths[new Date(2016, 12, 1).valueOf() / DIVIDER] = 'Dec';
     let marksYears = {
       1960: '1960',
       1985: '1985'
@@ -55,16 +58,16 @@ export class TimeSlider extends React.Component {
             Current Date Time:
           </ControlLabel>
           <FormGroup className={classes['InlineFormGroup']}>
-            <FormControl disabled type="text" placeholder="Current" value={this.state.current} onChange={this._onChangedCurrentDate}/>
+            <FormControl type="text" placeholder="Current" value={this.state.current} onChange={this._onChangedCurrentDate}/>
           </FormGroup>
-          <FormGroup className={classes['InlineFormGroup']}>
+          {/*<FormGroup className={classes['InlineFormGroup']}>
             <FormControl componentClass="select" placeholder="Day" onChange={this._onSelectedDay}>
               <option value="">Day</option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
             </FormControl>
-          </FormGroup>
+          </FormGroup>*/}
           <FormGroup className={classes['InlineFormGroup']}>
             <FormControl componentClass="select" placeholder="Hour" onChange={this._onSelectedHour}>
               <option value="">Hr.</option>
@@ -84,13 +87,20 @@ export class TimeSlider extends React.Component {
           </FormGroup>
         </Form>
         <Col sm={12}>
-          <Slider className={classes['SliderMonths']}
-                  min={1}
-                  max={12}
+          <Slider tipFormatter={(v) => {
+                    let date = new Date(v * 1000); // * 1000 To divide by 1000 the number of values
+                    // console.log(date); //Logged date is 1 month early than what's returned by date.getMonth() (????)
+                    // Same problem with moment.js
+                    return ((date.getMonth() === 0)? "12": date.getMonth()) +"/" + date.getDate();
+                  }}
+                  className={classes['SliderMonths']}
+                  min={new Date(2016, 1, 1).valueOf()/DIVIDER}
+                  max={new Date(2016, 12, 31).valueOf()/DIVIDER}
                   marks={marksMonths}
                   included={false}
                   range={true}
                   defaultValue={[5, 8]}
+                  onChange={this._onChangedMonthSlider}
           />
         </Col>
         <Col sm={12}>
@@ -100,6 +110,7 @@ export class TimeSlider extends React.Component {
                   marks={marksYears}
                   included={false}
                   defaultValue={1960}
+                  onChange={this._onChangedYearSlider}
           />
         </Col>
         <Form className={classes['StepControls']} inline>
@@ -163,6 +174,28 @@ export class TimeSlider extends React.Component {
          </Form>
       </div>
     )
+  }
+
+  _onChangedMonthSlider(values) {
+    if(values[0]){
+      //Datetime range
+      let dates = [
+        new Date(values[0] * DIVIDER),
+        new Date(values[1] * DIVIDER)
+      ];
+      console.log("[" +
+        (((dates[0].getMonth() === 0)? "12": dates[0].getMonth()) +"/" + dates[0].getDate()) + ", " +
+        (((dates[1].getMonth() === 0)? "12": dates[1].getMonth()) +"/" + dates[1].getDate()) + "]");
+    }else{
+      // Only one
+      let date = new Date(value * DIVIDER);
+      console.log(((date.getMonth() === 0)? "12": date.getMonth()) +"/" + date.getDate());
+    }
+
+  }
+
+  _onChangedYearSlider(value) {
+    console.log(value);
   }
 
   _onChangedCurrentDate (event) {
