51a94cb36166cdc4fa2ce2878338b2e300c8b56e
Félix Gagnon-Grenier <fractalesque@gmail.com>
2016-12-02 17:28:34 -0500

    PAV-300 - feature - ag-grid fed from props

--- src/components/Monitor/JobTable.js
+++ src/components/Monitor/JobTable.js
@@ -1,25 +1,51 @@
-import {Panel, Grid, Row, Col, Button} from 'react-bootstrap';
+import {Panel, Grid, Row, Col} from 'react-bootstrap';
+import {AgGridReact} from 'ag-grid-react';
+import './../../../node_modules/ag-grid/dist/styles/ag-grid.css';
+import './../../../node_modules/ag-grid/dist/styles/theme-bootstrap.css';
+import classes from './Monitor.scss';
 import React from 'react';
+import LinkCellRenderer from './LinkCellRenderer';
 class JobTable extends React.Component {
   static propTypes = {
     jobs: React.PropTypes.array.isRequired
   };
+
+  columnDefs () {
+    return [
+      {headerName: 'Process Identifier', field: 'title'},
+      {headerName: 'Status', field: 'status'},
+      {headerName: 'Duration', field: 'duration'},
+      {headerName: 'Result (xml)', field: 'status_location', cellRendererFramework: LinkCellRenderer}
+    ];
+  }
+
+  // please leave there even if ide says it's unused
+  // it actually ensures columns are fit
+  componentDidUpdate = () => {
+    if (this.api) {
+      this.api.sizeColumnsToFit();
+    }
+  };
+
+  onGridReady = (params) => {
+    this.api = params.api;
+  };
+
   render () {
     return (
-      <Grid>
+      <Grid className={classes.Monitor}>
         <Row>
           <Col mdOffset={2} md={8}>
             <Panel header="Jobs">
-              {
-                this.props.jobs.map((job, i) => {
-                  return (
-                    <div key={i}>
-                      <Button onClick="">Visualize</Button>
-                      {job.title} - <a href={job.status_location}>results</a>
-                    </div>
-                  );
-                })
-              }
+              <div className={classes.agGrid + ' ag-bootstrap'}>
+                <AgGridReact
+                  onGridReady={this.onGridReady}
+                  className={classes.agGrid}
+                  rowData={this.props.jobs}
+                  columnDefs={this.columnDefs()}
+                  enableSorting
+                />
+              </div>
             </Panel>
           </Col>
         </Row>
