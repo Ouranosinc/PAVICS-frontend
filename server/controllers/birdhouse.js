29fbe504242b70af3c3b22eb4d646dd1a03ffbec
Félix Gagnon-Grenier <fractalesque@gmail.com>
2016-12-12 14:53:43 -0500

    PAV-301 - fix - linting

--- server/controllers/birdhouse.js
+++ server/controllers/birdhouse.js
@@ -1,29 +1,32 @@
 'use strict';
-import config from '../../config'
-import { parseString } from 'xml2js'
-import request from 'koa-request'
-
-//Explanation here http://blog.stevensanderson.com/2013/12/21/experiments-with-koa-and-javascript-generators/
-function parseXMLThunk(response){
-  //console.log(response);
-  return function(callback) {
+import config from '../../config';
+import {parseString} from 'xml2js';
+import request from 'koa-request';
+// Explanation here http://blog.stevensanderson.com/2013/12/21/experiments-with-koa-and-javascript-generators/
+function parseXMLThunk (response) {
+  // console.log(response);
+  return function (callback) {
     parseXMLAsync(response, callback);
   };
 }
-
-function parseXMLAsync(response, callback){
+function parseXMLAsync (response, callback) {
   parseString(response, function (err, result) {
-    //console.log(result);
+    if (err) {
+      console.log(err);
+    }
+    // console.log(result);
     callback(null, result);
   });
 }
-
-module.exports.getCapabilities = function * list(next) {
-  if ('GET' != this.method) return yield next;
-  var options = {
+module.exports.getCapabilities = function * list (next) {
+  if (this.method !== 'GET') {
+    return yield next;
+  }
+  let options = {
     url: config.pavics_birdhouse_path
   };
-  var response = yield request(options); //Yay, HTTP requests with no callbacks!
+  // Yay, HTTP requests with no callbacks!
+  let response = yield request(options);
   this.body = yield parseXMLThunk(response.body);
 };
 
