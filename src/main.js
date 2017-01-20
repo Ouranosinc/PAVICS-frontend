5ec374fd1c6cfbd7a41d0625fabc16da98c318a7
Félix Gagnon-Grenier <fractalesque@gmail.com>
2017-01-20 14:47:19 -0500

    fixing lint, bumping version to fix tap event error

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
-import { useRouterHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
-import createStore from './store/createStore'
-import AppContainer from './containers/AppContainer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import {useRouterHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
+import createStore from './store/createStore';
+import AppContainer from './containers/AppContainer';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// ========================================================
+// Fix for mobile tap event, waiting for material-ui to phase towards onClick instead of onTap
+// ========================================================
+injectTapEventPlugin();
 
 // ========================================================
 // Browser History Setup
 // ========================================================
 const browserHistory = useRouterHistory(createBrowserHistory)({
   basename: __BASENAME__
-})
-
+});
 // ========================================================
 // Store and History Instantiation
 // ========================================================
@@ -20,29 +25,25 @@ const browserHistory = useRouterHistory(createBrowserHistory)({
 // react-router-redux reducer under the routerKey "router" in src/routes/index.js,
 // so we need to provide a custom `selectLocationState` to inform
 // react-router-redux of its location.
-const initialState = window.___INITIAL_STATE__
-const store = createStore(initialState, browserHistory)
+const initialState = window.___INITIAL_STATE__;
+const store = createStore(initialState, browserHistory);
 const history = syncHistoryWithStore(browserHistory, store, {
   selectLocationState: (state) => state.router
-})
-
+});
 // ========================================================
 // Developer Tools Setup
 // ========================================================
 if (__DEBUG__) {
   if (window.devToolsExtension) {
-    window.devToolsExtension.open()
+    window.devToolsExtension.open();
   }
 }
-
 // ========================================================
 // Render Setup
 // ========================================================
-const MOUNT_NODE = document.getElementById('root')
-
+const MOUNT_NODE = document.getElementById('root');
 let render = (routerKey = null) => {
-  const routes = require('./routes/index').default(store)
-
+  const routes = require('./routes/index').default(store);
   ReactDOM.render(
     <AppContainer
       store={store}
@@ -51,29 +52,26 @@ let render = (routerKey = null) => {
       routerKey={routerKey}
     />,
     MOUNT_NODE
-  )
-}
-
+  );
+};
 // Enable HMR and catch runtime errors in RedBox
 // This code is excluded from production bundle
 if (__DEV__ && module.hot) {
-  const renderApp = render
+  const renderApp = render;
   const renderError = (error) => {
-    const RedBox = require('redbox-react')
-
-    ReactDOM.render(<RedBox error={error} />, MOUNT_NODE)
-  }
+    const RedBox = require('redbox-react');
+    ReactDOM.render(<RedBox error={error} />, MOUNT_NODE);
+  };
   render = () => {
     try {
-      renderApp(Math.random())
+      renderApp(Math.random());
     } catch (error) {
-      renderError(error)
+      renderError(error);
     }
-  }
-  module.hot.accept(['./routes/index'], () => render())
+  };
+  module.hot.accept(['./routes/index'], () => render());
 }
-
 // ========================================================
 // Go!
 // ========================================================
-render()
+render();
