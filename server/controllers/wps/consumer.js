f56af7458b128df56defe28fb3b747e5522d6ec0
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2017-02-28 14:52:37 -0500

    PAV-169 - parameterizing opendap url in query to koa

--- server/controllers/wps/consumer.js
+++ server/controllers/wps/consumer.js
@@ -21,7 +21,6 @@ var consumer = (function () {
       let response;
       let url;
       let xml;
-      let jsonPath;
       switch (this.params.identifier) {
         case 'pavicsearch':
           // url = config.pavics_pywps_path + urlEncode(this.request.query);
@@ -36,18 +35,17 @@ var consumer = (function () {
           this.body = response.body;
           break;
         case 'plotly':
-          url = 'http://132.217.140.45:8009/pywps?service=WPS&request=execute&version=1.0.0' +
-            '&identifier=ncplotly&DataInputs=opendap_url=' +
-            'http://132.217.140.45:8083/thredds/dodsC/birdhouse/ouranos/subdaily/aet/pcp/aet_pcp_1970.nc;' +
-            'variable_name=' + this.request.query.variable_name +
-            ';time_initial_indice=' + this.request.query.time_initial_indice +
-            ';time_final_indice=' + this.request.query.time_final_indice +
-            ';spatial1_initial_indice=' + this.request.query.spatial1_initial_indice +
-            ';spatial1_final_indice=' + this.request.query.spatial1_final_indice +
-            ';spatial2_initial_indice=' + this.request.query.spatial2_initial_indice +
-            ';spatial2_final_indice=' + this.request.query.spatial2_final_indice;
+          url = `${config.pavics_pywps_path}?service=WPS&request=execute&version=1.0.0` +
+            `&identifier=ncplotly&DataInputs=opendap_url=${this.request.query['opendap_url']}` +
+            `;variable_name=${this.request.query['variable_name']}` +
+            `;time_initial_indice=` + this.request.query['time_initial_indice'] +
+            `;time_final_indice=` + this.request.query['time_final_indice'] +
+            `;spatial1_initial_indice=` + this.request.query['spatial1_initial_indice'] +
+            `;spatial1_final_indice=` + this.request.query['spatial1_final_indice'] +
+            `;spatial2_initial_indice=` + this.request.query['spatial2_initial_indice'] +
+            `;spatial2_final_indice=` + this.request.query['spatial2_final_indice'];
+          console.log('fetching plotly data:', url);
           response = yield request(url);
-          console.log(url);
           xml = yield Utils.parseXMLThunk(response.body);
           jsonPath = Utils.extractWPSOutputPath(xml);
           response = yield request(jsonPath);
