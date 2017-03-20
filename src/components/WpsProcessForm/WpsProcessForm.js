e980a13fe6648d9883b00a2717495327f9e16e41
Félix Gagnon-Grenier <fractalesque@gmail.com>
2017-03-20 16:17:23 -0400

    PAV-325 - editing resources manually and from layer switcher

--- src/components/WpsProcessForm/WpsProcessForm.js
+++ src/components/WpsProcessForm/WpsProcessForm.js
@@ -6,25 +6,14 @@ import classes from '../WorkflowWizard/WorkflowWizard.scss';
 const BOOLEAN = '//www.w3.org/TR/xmlschema-2/#boolean';
 const STRING = '//www.w3.org/TR/xmlschema-2/#string';
 const NETCDF = 'ComplexData';
-function getXhr () {
-  return new XMLHttpRequest();
-}
-function makePostRequest (url, data, callable, params) {
-  let xhr = getXhr();
-  xhr.onload = function () {
-    if (callable !== undefined) {
-      callable(xhr.responseText, params);
-    }
-  };
-  xhr.open('POST', url);
-  xhr.setRequestHeader('accept', 'text/html');
-  xhr.send(data);
-}
+const LABEL_NETCDF = 'LABEL_NETCDF';
+const LABEL_SHAPEFILE = 'LABEL_SHAPEFILE';
 export default class WpsProcessForm extends React.Component {
   static propTypes = {
     selectedProcess: React.PropTypes.object.isRequired,
     selectedProcessInputs: React.PropTypes.array.isRequired,
     selectedProcessValues: React.PropTypes.object.isRequired,
+    // keep the handleSelected[...] func for now, might use it later
     handleSelectedProcessValueChange: React.PropTypes.func.isRequired,
     executeProcess: React.PropTypes.func.isRequired,
     selectedProvider: React.PropTypes.string.isRequired,
@@ -32,44 +21,105 @@ export default class WpsProcessForm extends React.Component {
     selectedShapefile: React.PropTypes.object.isRequired,
     selectedDatasetLayer: React.PropTypes.object.isRequired
   };
-  handleChange = (event) => {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      formData: {}
+    };
+    this.execute = this.execute.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.makePostRequest = this.makePostRequest.bind(this);
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.selectedDatasetLayer['urls']) {
+      // TODO when unhardcoding files, remove the [0]
+      if (this.state.formData[LABEL_NETCDF] !== nextProps.selectedDatasetLayer['urls'][0]) {
+        this.setState({
+          ...this.state,
+          formData: {
+            ...this.state.formData,
+            [LABEL_NETCDF]: nextProps.selectedDatasetLayer['urls'][0]
+          }
+        });
+      }
+    }
+    if (nextProps.selectedShapefile['wmsParams']) {
+      if (this.state.formData[LABEL_SHAPEFILE] !== nextProps.selectedShapefile['wmsParams']['LAYERS']) {
+        this.setState({
+          ...this.state,
+          formData: {
+            ...this.state.formData,
+            [LABEL_SHAPEFILE]: nextProps.selectedShapefile['wmsParams']['LAYERS']
+          }
+        });
+      }
+    }
+  }
+
+  makePostRequest (url, data, callable, params) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      if (callable !== undefined) {
+        callable(xhr.responseText, params);
+      }
+    };
+    xhr.open('POST', url);
+    xhr.setRequestHeader('accept', 'text/html');
+    xhr.send(data);
+  }
+
+  handleChange (event) {
+    // TODO eventually this will probably go in the global state
+    // so use the handleProcessFOrmValueChange func passed in props
     let elem = event.target;
-    this.props.handleSelectedProcessValueChange(elem.id, elem.value);
-  };
-  execute = () => {
+    this.setState({
+      ...this.state,
+      formData: {
+        ...this.state.formData,
+        [elem.id]: elem.value
+      }
+    });
+  }
+
+  execute () {
     // ugly hack to workaround making one extra trip to the backend
     // we already have had to put strange __start__ and __end__ inputs to work nicely with phoenix
     let formData = new FormData(document.querySelector('#process-form'));
     let url = `${__PAVICS_PHOENIX_PATH__}/processes/execute?wps=${this.props.selectedProvider}&process=${this.props.selectedProcess.identifier}`;
     // let url = `/phoenix/execute?wps=${this.props.selectedProvider}&process=${this.props.selectedProcess.identifier}`;
-    makePostRequest(url, formData, (res) => {
+    this.makePostRequest(url, formData, (res) => {
+      // TODO actually do something once the post have been done
       console.log(res);
     });
     // this.props.executeProcess(provider, identifier, values);
     // this.props.goToSection(constants.PLATFORM_SECTION_MONITOR);
-  };
+  }
 
   makeInput (input) {
     switch (input.dataType) {
       case BOOLEAN:
         return (
           <div>
-            <FormControl bsClass={classes.checkbox} id={input.name} type="checkbox" name={input.name}
-              onChange={this.handleChange} />
+            <FormControl
+              bsClass={classes.checkbox}
+              id={input.name}
+              type="checkbox"
+              name={input.name} />
             <p>{input.description}</p>
           </div>
         );
       case NETCDF:
-        console.log('file:', this.props.selectedDatasetLayer);
         return (
           <div>
             <input type="hidden" name="__start__" value="resource:sequence" />
             <FormControl
               name="resource"
-              id={input.name}
+              id={LABEL_NETCDF}
               type="text"
               onChange={this.handleChange}
-              value={this.props.selectedDatasetLayer['opendap_urls'] ? this.props.selectedDatasetLayer['opendap_urls'][0] : ''}/>
+              value={this.state.formData[LABEL_NETCDF]} />
             <input type="hidden" name="__end__" value="resource:sequence" />
             <p>{input.description}</p>
           </div>
@@ -79,25 +129,35 @@ export default class WpsProcessForm extends React.Component {
           return (
             <div>
               <FormControl
-                id={input.name}
+                id={LABEL_SHAPEFILE}
                 type="text"
                 name={input.name}
                 onChange={this.handleChange}
-                value={this.props.selectedShapefile['wmsParams'] ? this.props.selectedShapefile['wmsParams']['LAYERS'] : ''} />
+                value={this.state.formData[LABEL_SHAPEFILE]} />
               <p>{input.description}</p>
             </div>
           );
         }
         return (
           <div>
-            <FormControl id={input.name} type="text" name={input.name} onChange={this.handleChange} />
+            <FormControl
+              id={input.name}
+              type="text"
+              name={input.name}
+              value={this.state.formData[input.name]}
+              onChange={this.handleChange} />
             <p>{input.description}</p>
           </div>
         );
       default:
         return (
           <div>
-            <FormControl id={input.name} type="text" name={input.name} onChange={this.handleChange} />
+            <FormControl
+              id={input.name}
+              type="text"
+              name={input.name}
+              value={this.state.formData[input.name]}
+              onChange={this.handleChange} />
             <p>{input.description}</p>
           </div>
         );
@@ -107,9 +167,13 @@ export default class WpsProcessForm extends React.Component {
   // the form needs a submit named input to actually be executed by phoenix
   // so 1990
   render () {
+    // TODO validate that async is really something we want each time
     return (
       <Paper zDepth={2}>
         <Form id="process-form" horizontal>
+          <input type="hidden" name="_charset_" value="UTF-8" />
+          <input type="hidden" name="__formid__" value="deform" />
+          <input type="hidden" name="_async_check" value="true" />
           {
             this.props.selectedProcessInputs.map((elem, i) => {
               return (
