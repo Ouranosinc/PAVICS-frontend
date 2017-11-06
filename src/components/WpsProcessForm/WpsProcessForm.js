328bb09b6ab2e59e6846c28f44b4541a0a7d3a6f
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2017-11-06 10:37:47 -0500

    PAV-419 - putting inputs that have multiple possible values in a sequence

--- src/components/WpsProcessForm/WpsProcessForm.js
+++ src/components/WpsProcessForm/WpsProcessForm.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import Checkbox from 'material-ui/Checkbox';
 import TextField from 'material-ui/TextField';
-import classes from '../WorkflowWizard/WorkflowWizard.scss';
 import DeformWrapper from '../DeformWrapper/DeformWrapper';
 
-const BOOLEAN = '//www.w3.org/TR/xmlschema-2/#boolean';
-const STRING = '//www.w3.org/TR/xmlschema-2/#string';
+/*
+Wps Process Form
+this module builds and updates the values of the inputs of a simple wps process
+it uses the custom routes of phoenix "api" to fetch the inputs of a specified provider and process
+
+it seems the dataType property of the inputs might change unpredictably (we have seen three forms to date) but they all seem to end with the type
+hence, for string and boolean, implement a type of "endsWith" check instead of pure equivalence
+
+We need to be able to automatically fill some of the fields as well as represent them in a meaningful way
+ - for dataType ComplexData and defaultValue.mimeType being application/x-netcdf, fill the field with the url property of the selected dataset
+   if there are multiple urls in the selected dataset, we should populate one input for each urls
+ - for dataType string and name resource, fill the field with the opendap_url of the selected dataset
+ - for dataType string and input.allowedValues.length > 0, make a select with the allowedValues.
+   if the maxOccurs is more than one the user might want to choose multiple values, so they should be allowed to
+   if maxOccurs is one, then only one value is allowd, so a simple select will be enough
+ - for dataType boolean make a checkbox
+ - if minOccurs is 0, the input is optional, if it's one, it should be required to submit the form
+ - for dataType dateTime create a date picker
+
+the inputs of the selected process are available in props.workflow.selectedProcessInputs
+
+ */
+
+// foreach inputs
+//   if minOccurs = 0 mark as optional
+//   if maxOccurs > 1 trigger rendering with start end
+//   create html input
+
+const BOOLEAN = 'boolean';
+const STRING = 'string';
 const NETCDF = 'ComplexData';
 const LABEL_NETCDF = 'LABEL_NETCDF';
 const LABEL_SHAPEFILE = 'LABEL_SHAPEFILE';
@@ -61,6 +88,7 @@ export default class WpsProcessForm extends React.Component {
       }
     }
     if (nextProps.selectedShapefile['wmsParams']) {
+      console.log('some things at least still work, next layer written should be %s', nextProps.selectedShapefile['wmsParams']['LAYERS']);
       if (this.state.formData[LABEL_SHAPEFILE] !== nextProps.selectedShapefile['wmsParams']['LAYERS']) {
         this.setState({
           ...this.state,
@@ -112,7 +140,8 @@ export default class WpsProcessForm extends React.Component {
     });
   }
 
-  makeInnerInput (input) {
+  makeInputMarkup (input) {
+
     switch (input.dataType) {
       case BOOLEAN:
         let value = false;
@@ -152,6 +181,7 @@ export default class WpsProcessForm extends React.Component {
         );
       case STRING:
         if (input.name === 'typename') {
+          console.log('we have at least one typename');
           return (
             <div>
               <TextField
@@ -206,31 +236,27 @@ export default class WpsProcessForm extends React.Component {
     }
   }
 
-  makeInput (input) {
-    console.log('making react component for input %o', input);
-    return (
-      <div>
-        {input['maxOccurs'] > 1 ? <input type="hidden" name="__start__" value="resource:sequence" /> : ''}
-        {this.makeInnerInput(input)}
-        {input['maxOccurs'] > 1 ? <input type="hidden" name="__end__" value="resource:sequence" /> : ''}
-      </div>
-    );
-  }
-
+  /*
+  deform is a library used by phoenix to manage it's different variable forms
+  this routine should be creating an html form adapted to the specific ways in which deform creates forms,
+  the less intuitive being the interesting __start__ and __end__ inputs around similarly named inputs
+  to create a sequence of values. the order is important.
+   */
   render () {
     return (
       <DeformWrapper
         formId={this.props.formId}
         execute={this.props.executeProcess}>
-        {
-          this.props.workflow.selectedProcessInputs.map((elem, i) => {
-            return (
-              <div key={i}>
-                {this.makeInput(elem)}
-              </div>
-            );
-          })
-        }
+        {this.props.workflow.selectedProcessInputs.map((input, i) => {
+          console.log('making react component for input %o', input);
+          return (
+            <div key={i}>
+              {input['maxOccurs'] > 1 ? <input type="hidden" name="__start__" value="resource:sequence" /> : ''}
+              {this.makeInputMarkup(input)}
+              {input['maxOccurs'] > 1 ? <input type="hidden" name="__end__" value="resource:sequence" /> : ''}
+            </div>
+          );
+        })}
       </DeformWrapper>
     );
   }
