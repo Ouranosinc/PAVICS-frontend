370e6d12be54da0fefb181abf5cf3d5eddacb0e9
Renaud009 <renaud.hebert-legault@crim.ca>
2017-12-01 20:25:44 +0000

    PAv-451 - Use the same ConfirmModal when deleting a workflow

--- src/components/ScientificWorkflowList/ScientificWorkflowList.js
+++ src/components/ScientificWorkflowList/ScientificWorkflowList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
-import Dialog from 'material-ui/Dialog';
 import CircularProgress from 'material-ui/CircularProgress';
 import { List, ListItem } from 'material-ui/List';
 import IconMenu from 'material-ui/IconMenu';
@@ -15,6 +14,8 @@ import Build from 'material-ui/svg-icons/action/build';
 import DeviceHub from 'material-ui/svg-icons/hardware/device-hub';
 import Pagination from '../Pagination';
 import * as constants from '../../constants';
+import ConfirmDialog from './../../components/ConfirmDialog';
+
 const style = {
   noWorkflows: {
     padding: '20px'
@@ -30,55 +31,32 @@ export default class ScientificWorkflowList extends Component {
   constructor (props) {
     super(props);
     this.onPageChanged = this.onPageChanged.bind(this);
-    this.openConfirmWorkflowDeletionDialog = this.openConfirmWorkflowDeletionDialog.bind(this);
-    this.makeConfirmWorkflowDeletionDialogActions = this.makeConfirmWorkflowDeletionDialogActions.bind(this);
-    this.closeDialog = this.closeDialog.bind(this);
-    this.onRunWorkflowClick = this.onRunWorkflowClick.bind(this);
-    this.onEditWorkflowClick = this.onEditWorkflowClick.bind(this);
-    this.confirmWorkflowDeletion = this.confirmWorkflowDeletion.bind(this);
+    this._openConfirmWorkflowDeletionDialog = this._openConfirmWorkflowDeletionDialog.bind(this);
+    this._onConfirmedWorkflowDeletion = this._onConfirmedWorkflowDeletion.bind(this);
+    this._onRunWorkflowClicked = this._onRunWorkflowClicked.bind(this);
+    this._onEditWorkflowClicked = this._onEditWorkflowClicked.bind(this);
     this.state = {
       pageNumber: 1,
       numberPerPage: constants.PER_PAGE_OPTIONS[constants.PER_PAGE_INITIAL_INDEX],
       dialogOpened: false,
       dialogContent: '',
-      dialogActions: []
+      dialogResource: null
     };
   }
 
-  openConfirmWorkflowDeletionDialog (id, name) {
+  _openConfirmWorkflowDeletionDialog (workflow) {
     this.setState({
       dialogOpened: true,
-      dialogContent: 'Do you really want to delete the workflow ' + name + '?',
-      dialogActions: this.makeConfirmWorkflowDeletionDialogActions(id)
+      dialogContent: 'Do you really want to delete the workflow ' +  workflow.json.name + '?',
+      dialogResource: workflow
     });
   }
 
-  closeDialog () {
+  _onConfirmedWorkflowDeletion (workflow) {
+    this.props.workflowAPIActions.deleteWorkflow({ id: workflow.id });
     this.setState({
       dialogOpened: false
     });
-  }
-
-  confirmWorkflowDeletion (id) {
-    this.props.workflowAPIActions.deleteWorkflow({ id: id });
-    this.closeDialog();
-  }
-
-  makeConfirmWorkflowDeletionDialogActions (id) {
-    return [
-      <FlatButton
-        label="Cancel"
-        primary={false}
-        keyboardFocused={true}
-        onTouchTap={this.closeDialog}
-      />,
-      <FlatButton
-        label="OK"
-        primary={true}
-        keyboardFocused={false}
-        onTouchTap={() => {this.confirmWorkflowDeletion(id);}}
-      />
-    ]
   }
 
 
@@ -89,12 +67,12 @@ export default class ScientificWorkflowList extends Component {
     });
   }
 
-  onRunWorkflowClick (workflow) {
+  _onRunWorkflowClicked (workflow) {
     // Deep clone workflow object so modifications (provider names, url, inputs) won't affect it's state if reloaded from the list
     this.props.goToConfigureAndRunStep(JSON.parse(JSON.stringify(workflow)));
   }
 
-  onEditWorkflowClick () {
+  _onEditWorkflowClicked () {
     this.setState({
       dialogOpened: true,
       dialogContent: 'editing workflow'
@@ -116,23 +94,25 @@ export default class ScientificWorkflowList extends Component {
               {
                 paginated.map((workflow, i) => {
                   return (
-                    <ListItem
-                      style={{width: '98%'}}
-                      primaryText={workflow.json.name || workflow.json}
-                      leftIcon={<DeviceHub />} key={i}
-                      rightIconButton={
-                        <IconMenu iconButtonElement={
-                          <IconButton
-                            touch={true}
-                            tooltip="Actions"
-                            tooltipPosition="bottom-left">
-                            <MoreVertIcon color={grey400} />
-                          </IconButton>}>
-                          <MenuItem leftIcon={<Build />} onTouchTap={() => this.onRunWorkflowClick(workflow)}>Configure & Run</MenuItem>
-                          <MenuItem leftIcon={<Create />} onTouchTap={this.onEditWorkflowClick}>Edit</MenuItem>
-                          <MenuItem leftIcon={<Remove />} onTouchTap={() => {this.openConfirmWorkflowDeletionDialog(workflow.id, workflow.json.name)}}>Delete</MenuItem>
-                          </IconMenu>
-                      }/>
+                    <div>
+                      <ListItem
+                        style={{width: '98%'}}
+                        primaryText={workflow.json.name || workflow.json}
+                        leftIcon={<DeviceHub />} key={i}
+                        rightIconButton={
+                          <IconMenu iconButtonElement={
+                            <IconButton
+                              touch={true}
+                              tooltip="Actions"
+                              tooltipPosition="bottom-left">
+                              <MoreVertIcon color={grey400} />
+                            </IconButton>}>
+                            <MenuItem leftIcon={<Build />} onTouchTap={() => this._onRunWorkflowClicked(workflow)}>Configure & Run</MenuItem>
+                            <MenuItem leftIcon={<Create />} onTouchTap={this._onEditWorkflowClicked}>Edit</MenuItem>
+                            <MenuItem leftIcon={<Remove />} onTouchTap={() => {this._openConfirmWorkflowDeletionDialog(workflow)}}>Delete</MenuItem>
+                            </IconMenu>
+                        }/>
+                      </div>
                   );
                 })
               }
@@ -142,15 +122,13 @@ export default class ScientificWorkflowList extends Component {
               initialPerPageOptionIndex={constants.PER_PAGE_INITIAL_INDEX}
               perPageOptions={constants.PER_PAGE_OPTIONS}
               onChange={this.onPageChanged}/>
+            <ConfirmDialog
+              isOpen={this.state.dialogOpened}
+              affectedResource={this.state.dialogResource}
+              onDialogConfirmed={this._onConfirmedWorkflowDeletion}
+              dialogContent={this.state.dialogContent}>
+            </ConfirmDialog>
           </Paper>
-          <Dialog
-            title="Attention"
-            modal={false}
-            open={this.state.dialogOpened}
-            onRequestClose={this.closeDialog}
-            actions={this.state.dialogActions}>
-            {this.state.dialogContent}
-          </Dialog>
         </div>
       );
     } else {
