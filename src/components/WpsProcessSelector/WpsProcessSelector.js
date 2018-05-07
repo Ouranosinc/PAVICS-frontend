5e70312910c66676d870ff863e9610766a7cbe3c
Renaud009 <renaud.hebert-legault@crim.ca>
2018-05-07 21:13:49 +0100

    Filter processes by keyword

--- src/components/WpsProcessSelector/WpsProcessSelector.js
+++ src/components/WpsProcessSelector/WpsProcessSelector.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {Card, CardHeader, CardActions, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+
 export default class WpsProcessSelector extends React.Component {
   static propTypes = {
     workflow: React.PropTypes.object.isRequired,
     workflowActions: React.PropTypes.object.isRequired
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchKeyWord: ''
+    }
+  }
 
   makeChooseProcessCallback (process) {
     return () => {
@@ -20,11 +30,29 @@ export default class WpsProcessSelector extends React.Component {
       'overflowY': 'auto',
       'margin': '10px 0'
     };
+    let filteredProcesses = this.props.workflow.processes;
+    if (this.state.searchKeyWord.length) {
+      filteredProcesses = this.props.workflow.processes.filter((process) => {
+        return process.identifier.toUpperCase().includes(this.state.searchKeyWord.toUpperCase()) ||
+          process.title.toUpperCase().includes(this.state.searchKeyWord.toUpperCase()) ||
+          process.description.toUpperCase().includes(this.state.searchKeyWord.toUpperCase());
+      });
+    }
     return (
       <div>
+        <Paper style={{marginTop: 20}}>
+          <div className="container">
+            <TextField
+              id="cy-filter-by-keyword"
+              hintText="Filter list by keyword"
+              fullWidth={true}
+              onChange={(event, value) => this.setState({searchKeyWord: value})}
+              floatingLabelText="Filter by" />
+          </div>
+        </Paper>
         <div style={gridStyle}>
           {
-            this.props.workflow.processes.map((process, i) => {
+            filteredProcesses.map((process, i) => {
               return (
                 <Card key={i}>
                   <CardHeader
