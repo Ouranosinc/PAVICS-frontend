d86561efbbf0c71400a67e812d246e3b83ff9fb9
Renaud009 <renaud.hebert-legault@crim.ca>
2018-05-07 21:15:25 +0100

    Fix input form (re)loading with formData and auto-fill of workflow form inputs after selections

--- src/components/WpsProcessForm/WpsProcessForm.js
+++ src/components/WpsProcessForm/WpsProcessForm.js
@@ -61,18 +61,25 @@ export default class WpsProcessForm extends React.Component {
 
   constructor (props) {
     super(props);
-    let formData = {};
-    this.props.workflow.selectedProcessInputs.forEach((input) => {
-      formData[makeUniqueIdentifier(input)] = input.defaultValue || '';
-    });
     this.state = {
-      formData: formData
+      formData: {}
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount () {
+    this.buildFormData(this.props);
     this.verifyMeaningfulValues(this.props);
+  }
+
+  buildFormData(props) {
+    let formData = {};
+    props.workflow.selectedProcessInputs.forEach((input) => {
+      formData[makeUniqueIdentifier(input)] = input.defaultValue || '';
+    });
+    this.state = {
+      formData: formData
+    };
   }
 
   /*
@@ -100,27 +107,40 @@ export default class WpsProcessForm extends React.Component {
     console.log('verifying meaningful values in %o, current state: %o', props, this.state);
 
     let changedState = {};
+    // For each input, if any has the right mimetype and name, propagate new information
+    for(let inputName in this.state.formData) {
+      if (inputName.startsWith(constants.LABEL_NETCDF.split('.')[0]) && inputName.endsWith(constants.LABEL_NETCDF.split('.')[1])) {
+        if (props.currentDisplayedDataset['url']) {
+          changedState[inputName] = props.currentDisplayedDataset['url'];
+        } else {
+          changedState[inputName] = '';
+        }
+      }
 
-    if (props.currentDisplayedDataset['url']) {
-      changedState[constants.LABEL_NETCDF] = props.currentDisplayedDataset['url'];
-    } else {
-      changedState[constants.LABEL_NETCDF] = '';
-    }
+      if (inputName.startsWith(constants.LABEL_OPENDAP.split('.')[0]) && inputName.endsWith(constants.LABEL_OPENDAP.split('.')[1])) {
+        if (props.currentDisplayedDataset['opendap_url']) {
+          changedState[inputName] = props.currentDisplayedDataset['opendap_url'];
+        } else {
+          changedState[inputName] = '';
+        }
+      }
 
-    if (props.currentDisplayedDataset['opendap_url']) {
-      changedState[constants.LABEL_OPENDAP] = props.currentDisplayedDataset['opendap_url'];
-    } else {
-      changedState[constants.LABEL_OPENDAP] = '';
-    }
 
-    if (props.selectedShapefile['wmsParams'] && props.selectedShapefile['wmsParams']['LAYERS']) {
-      changedState[constants.LABEL_SHAPEFILE] = props.selectedShapefile['wmsParams']['LAYERS'];
-    } else {
-      changedState[constants.LABEL_SHAPEFILE] = '';
-    }
+      if (inputName.startsWith(constants.LABEL_SHAPEFILE.split('.')[0]) && inputName.endsWith(constants.LABEL_SHAPEFILE.split('.')[1])) {
+        if (props.selectedShapefile['wmsParams'] && props.selectedShapefile['wmsParams']['LAYERS']) {
+          changedState[inputName] = props.selectedShapefile['wmsParams']['LAYERS'];
+        } else {
+          // changedState[inputName] = '';
+        }
+      }
 
-    if (props.selectedRegions) {
-      changedState[constants.LABEL_FEATURE_IDS] = props.selectedRegions;
+      if (inputName.startsWith(constants.LABEL_FEATURE_IDS.split('.')[0]) && inputName.endsWith(constants.LABEL_FEATURE_IDS.split('.')[1])) {
+        if (props.selectedRegions) {
+          changedState[inputName] = props.selectedRegions;
+        } else {
+          changedState[inputName] = []
+        }
+      }
     }
 
     this.setState({
@@ -133,6 +153,9 @@ export default class WpsProcessForm extends React.Component {
   }
 
   componentWillReceiveProps (nextProps) {
+    if(nextProps.workflow.selectedProcessInputs && this.props.workflow.selectedProcessInputs !== nextProps.workflow.selectedProcessInputs) {
+      this.buildFormData(nextProps)
+    }
     this.verifyMeaningfulValues(nextProps);
   }
 
