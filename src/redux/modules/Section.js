16178882d32c711c8e0e358074bb95f7c796baad
Renaud009 <renaud.hebert-legault@crim.ca>
2018-07-11 16:36:24 +0100

    Created visualize, section & widgets reducers and refactored code accordingly

---
+++ src/redux/modules/Section.js
@@ -0,0 +1,31 @@
+// Constants
+export const constants = {
+  PLATFORM_SET_SECTION: 'SECITON_MANAMGENT.PLATFORM_SET_SECTION'
+};
+
+// Action Creators
+export const actions = {
+  goToSection: function (section) {
+    return {
+      type: constants.PLATFORM_SET_SECTION,
+      openedSection: section
+    };
+  }
+};
+
+// Reducer
+const HANDLERS = {
+  [constants.PLATFORM_SET_SECTION]: (state, action) => {
+    return {...state, openedSection: action.openedSection};
+  }
+};
+
+// Initial State
+export const initialState = {
+  openedSection: ''
+};
+
+export default function (state = initialState, action) {
+  const handler = HANDLERS[action.type];
+  return handler ? handler(state, action) : state;
+}
---
+++ src/redux/modules/Widgets.js
@@ -0,0 +1,45 @@
+// Constants
+export const constants = {
+  TOGGLE_WIDGET: 'WIDGET.TOGGLE_WIDGET',
+  WIDGET_INFO_KEY: 'info',
+  WIDGET_MAP_CONTROLS_KEY: 'mapControls',
+  WIDGET_CHART_KEY: 'chart',
+  WIDGET_LAYER_SWITCHER_KEY: 'layerSwitcher',
+  WIDGET_TIME_SLIDER_KEY: 'timeSlider',
+};
+
+// Actions
+
+
+// Action Creators
+export const actions = {
+  toggleWidget: function (key) {
+    return {
+      type: constants.TOGGLE_WIDGET,
+      key: key
+    };
+  }
+};
+
+// Reducer
+const HANDLERS = {
+  [constants.TOGGLE_WIDGET]: (state, action) => {
+    const newState = {};
+    newState[action.key] = !state[action.key];
+    return {...state, ...newState};
+  }
+};
+
+// Initial State
+export const initialState = {
+  chart: false,
+  info: false,
+  mapControls: true,
+  timeSlider: true,
+  layerSwitcher: true
+};
+
+export default function (state = initialState, action) {
+  const handler = HANDLERS[action.type];
+  return handler ? handler(state, action) : state;
+}
