7436d1728d6b7ccdc89dec2f72d0b68565a5efba
Renaud009 <renaud.hebertlegault@gmail.com>
2018-10-25 21:31:26 +0100

    Missed conflict

--- src/containers/WidgetTimeSeries/WidgetTimeSeries.js
+++ src/containers/WidgetTimeSeries/WidgetTimeSeries.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-<<<<<<< HEAD
-import WidgetTimeSeries from './../../components/WidgetTimeSeries';
-import { actions as visualiseActions } from './../../redux/modules/Visualize';
-=======
 import { actions as visualizeActions } from '../../redux/modules/Visualize';
 import WidgetTimeSeries from './../../components/WidgetTimeSeries';
->>>>>>> drawn-features-to-binary-format
+
 
 const mapStateToProps = (state) => {
   return {
@@ -20,11 +16,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     ...bindActionCreators(
       {
-<<<<<<< HEAD
-        fetchPlotlyData: visualiseActions.fetchPlotlyData
-=======
         fetchPlotlyData: visualizeActions.fetchPlotlyData
->>>>>>> drawn-features-to-binary-format
       },
       dispatch,
     )
